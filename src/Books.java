b73b1a205b241376bdd75a0b9b483544769d4fad
Lenovo <Lenovo@LAPTOP-D1O9QGEF.local>
2023-07-13 22:46:58 +0300

    Books modeli oluşturuldu

---
+++ src/Books.java
@@ -0,0 +1,34 @@
+
+public class Books {
+	private String isim;
+	private int sayfaSayısı;
+	private String yayınEvi;
+	private String tur;
+	
+	public String getIsim() {
+		return isim;
+	}
+	public void setIsim(String isim) {
+		this.isim = isim;
+	}
+	public int getSayfaSayısı() {
+		return sayfaSayısı;
+	}
+	public void setSayfaSayısı(int sayfaSayısı) {
+		this.sayfaSayısı = sayfaSayısı;
+	}
+	public String getYayınEvi() {
+		return yayınEvi;
+	}
+	public void setYayınEvi(String yayınEvi) {
+		this.yayınEvi = yayınEvi;
+	}
+	public String getTur() {
+		return tur;
+	}
+	public void setTur(String tur) {
+		this.tur = tur;
+	}
+	
+	
+}
---
+++ src/Kutuphane.java
@@ -0,0 +1,9 @@
+
+public class Kutuphane {
+
+	public static void main(String[] args) {
+	
+
+	}
+
+}
